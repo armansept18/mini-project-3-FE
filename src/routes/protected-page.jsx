36a210a1f75b8709469800651a0ca58da8c67a1d
nazhifsetya27 <nazhif.nugroho@gmail.com>
2023-09-12 18:05:30 +0700

    Update protected-page.jsx

--- src/routes/protected-page.jsx
+++ src/routes/protected-page.jsx
@@ -5,8 +5,6 @@ import { useNavigate } from "react-router-dom";
 export const ProtectedPage = ({ children, needLogin = false }) => {
   const userSelector = useSelector((state) => state.auth);
   const nav = useNavigate();
-  console.log("children", children);
-  console.log("user selector", userSelector);
 
   useEffect(() => {
     if (needLogin && userSelector.role_id !== 1) {
