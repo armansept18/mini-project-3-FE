4e227ddc650ddff0731e23c31967165264e6ddbe
Arman Septian Pratama <armansept18@gmail.com>
2023-09-12 22:21:37 +0700

    feat: search bar and sorting bar origin

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,12 @@
+import { SearchBar } from "../../components/search-bar";
+import { SortingBar } from "../../components/sorting-product";
+
+
 export const HomePage = () => {
   return (
     <>
+      <SearchBar />
+      <SortingBar />
       <div>Home Page</div>
     </>
   );
