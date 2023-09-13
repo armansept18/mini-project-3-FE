aafa36f61831ea35ec560507af3a55e1958b9cce
Andre Riyanto <andreriyantoo19@gmail.com>
2023-09-13 13:42:17 +0700

    color fixing

--- src/components/slidebar/slidebar.jsx
+++ src/components/slidebar/slidebar.jsx
@@ -122,7 +122,7 @@ export const SlideBar = ({ isSlideOpen }) => {
                   className="text-2xl text-white"
                   onClick={() => setCollapseShow("hidden")}
                 />
-                <div class="flex justify-between border-b-4 ">
+                <div className="flex justify-between border-b-4 ">
                   {" "}
                   {/* <span className="hidden md:inline wi text-white">
                       ADMIN
@@ -136,9 +136,9 @@ export const SlideBar = ({ isSlideOpen }) => {
                 onClick={() => nav("/dashboard")}
                 className="cursor-pointer mb-4"
               >
-                <a class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg
-                    class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -147,7 +147,9 @@ export const SlideBar = ({ isSlideOpen }) => {
                     <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                   </svg>
-                  <span class="flex-1 ml-3 whitespace-nowrap">Dashboard</span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Dashboard
+                  </span>
                 </a>
               </li>
 
@@ -155,16 +157,16 @@ export const SlideBar = ({ isSlideOpen }) => {
                 onClick={() => nav("/product")}
                 className="cursor-pointer mb-4"
               >
-                <a class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <MdProductionQuantityLimits className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                  <span class="flex-1 ml-3 whitespace-nowrap">Product</span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Product</span>
                 </a>
               </li>
 
               <li className="mb-4">
-                <a class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg
-                    class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -172,17 +174,22 @@ export const SlideBar = ({ isSlideOpen }) => {
                   >
                     <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                   </svg>
-                  <span class="flex-1 ml-3 whitespace-nowrap">Employe</span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Employe</span>
                 </a>
               </li>
 
-              <li
-                onClick={() => nav("/product")}
-                className="cursor-pointer mb-4"
-              >
-                <a class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                  <MdProductionQuantityLimits className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                  <span class="flex-1 ml-3 whitespace-nowrap">Product</span>
+              <li className="mb-4">
+                <a className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 18"
+                  >
+                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                  </svg>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Employe</span>
                 </a>
               </li>
             </ul>
