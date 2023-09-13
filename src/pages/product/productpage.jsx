9383b04a093f7fd9a3273f350fa07dd773ca1f52
Andre Riyanto <andreriyantoo19@gmail.com>
2023-09-13 14:19:04 +0700

    progres chasier landing page

--- src/pages/product/productpage.jsx
+++ src/pages/product/productpage.jsx
@@ -2,22 +2,34 @@ import { useEffect, useState } from "react";
 import { CardProduct } from "../../components/cardproduct/productlist";
 import { NavTemplate } from "../../components/template/template";
 import api from "../../api/api";
+import { Pagination } from "react-bootstrap";
 
 export const ProductPage = () => {
   const [product, setProduct] = useState([]);
-  const fetchProduct = async () => {
-    await api.get("/products").then((result) => setProduct(result.data));
+  const fetchProduct = async (page, pageSize) => {
+    await api
+      .get("/products", { params: { page, pageSize } })
+      .then((result) => setProduct(result.data));
   };
 
+  const [currentPage, setCorrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const toalPages = Math.ceil(product.length / itemsPerPage);
+
+  const handleChangePage = () => {};
   useEffect(() => {
-    fetchProduct();
-  }, []);
+    fetchProduct(currentPage, itemsPerPage);
+  }, [currentPage, itemsPerPage]);
   return (
     <>
       <NavTemplate>
         <div class="col-auto items-center justify-center h-24 rounded max-md:mt-28 md:ml-72 md:max-w-5xl">
           <CardProduct product={product} />
         </div>
+        <Pagination>
+          <Pagination.Prev onClick={() => handleChangePage(currentPage - 2)} />
+        </Pagination>
       </NavTemplate>
     </>
   );
