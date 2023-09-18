1577262a83189f7767876b3c34b11fbcd7fbb5dc
nazhifsetya27 <nazhif.nugroho@gmail.com>
2023-09-18 13:47:01 +0700

    feat: transaction phase 2

--- src/components/cardproduct/cardproductcashier/cartContext.jsx
+++ src/components/cardproduct/cardproductcashier/cartContext.jsx
@@ -7,28 +7,71 @@ export function useCart() {
 }
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState({});
+  const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
-    setCart(item);
+    // Check if the item is already in the cart
+    const existingItemIndex = cart.findIndex(
+      (cartItem) => cartItem.id === item.id
+    );
+
+    if (existingItemIndex !== -1) {
+      // If the item is already in the cart, update its quantity
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex].quantity += 1;
+      setCart(updatedCart);
+    } else {
+      // If the item is not in the cart, add it
+      const updatedCart = [...cart, { ...item, quantity: 1 }];
+      setCart(updatedCart);
+    }
   };
+
+  const decrementCart = (itemId) => {
+    // Find the item in the cart
+    const existingItemIndex = cart.findIndex(
+      (cartItem) => cartItem.id === itemId
+    );
+
+    if (existingItemIndex !== -1) {
+      // If the item is in the cart, decrement its quantity
+      const updatedCart = [...cart];
+      if (updatedCart[existingItemIndex].quantity > 1) {
+        updatedCart[existingItemIndex].quantity -= 1;
+        setCart(updatedCart);
+      } else {
+        // If the quantity becomes 0, remove the item from the cart
+        updatedCart.splice(existingItemIndex, 1);
+        setCart(updatedCart);
+      }
+    }
+  };
+
+  const incrementCart = (itemId) => {
+    // Find the item in the cart
+    const existingItemIndex = cart.findIndex(
+      (cartItem) => cartItem.id === itemId
+    );
+
+    if (existingItemIndex !== -1) {
+      // If the item is in the cart, increment its quantity
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex].quantity += 1;
+      setCart(updatedCart);
+    }
+  };
+
+  function clearCart() {
+    setCart([]);
+  }
 
   useEffect(() => {
     console.log("cart", cart);
   }, [cart]);
 
-  const decrementCart = () => {};
-
-  const incrementCart = (item) => {
-    setCart((item.quantity += 1));
-    alert("success increment");
-  };
-
-  function submit() {}
-
   return (
     <CartContext.Provider
-      value={{ cart, addToCart, decrementCart, incrementCart }}
+      value={{ cart, addToCart, decrementCart, incrementCart, clearCart }}
     >
       {children}
     </CartContext.Provider>
--- src/components/cardtransaction/cardtransaction.jsx
+++ src/components/cardtransaction/cardtransaction.jsx
@@ -5,10 +5,65 @@ import {
   useCart,
 } from "../cardproduct/cardproductcashier/cartContext";
 import emptyCart from "../../assets/icons/cart.svg";
+import api from "../../api/api";
+import { useToast } from "@chakra-ui/react";
 
 export const CardTransaction = () => {
-  const { cart, incrementCart } = useCart();
-  console.log(cart);
+  const { cart, incrementCart, decrementCart, clearCart } = useCart();
+  // console.log(cart);
+  const toast = useToast();
+
+  async function submit() {
+    try {
+      // prepare data that are want to send to back end
+      const products = cart.map((cartItem) => ({
+        id: cartItem.id,
+        quantity: cartItem.quantity,
+      }));
+
+      const requestData = {
+        products: products,
+      };
+
+      const response = await api.post("/transactions/create", requestData);
+      if (response.status === 200) {
+        // Transaction created successfully
+        const responseData = response.data;
+        console.log("Transaction created:", responseData);
+        toast({
+          title: "Success",
+          description: "Success Create Transaction",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+        clearCart();
+      } else {
+        console.error("Transaction creation failed");
+        toast({
+          title: "Failed",
+          description: "Transaction creation failed",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+    } catch (error) {
+      console.error("Error creating transaction:", error);
+      toast({
+        title: "Error creating transaction",
+        description: `Cart Kosong!`,
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+        position: "top",
+      });
+    }
+  }
+
+  const isCartEmpty = cart.length === 0;
 
   return (
     <>
@@ -18,42 +73,57 @@ export const CardTransaction = () => {
           <span className="font-semibold text-lg">TRANSACTION</span>
         </div>
 
-        {/* cart */}
-        <div className=" flex justify-between items-center bg-gray-300 rounded-xl mt-2 p-4 md:w-full h-24">
-          {/* item 1 (photo) */}
-          {/* ternary here */}
-          <div className="flex-shrink-0">
-            <img src={cart.image} alt="pho" class="w-16 h-16 rounded-full" />
+        {isCartEmpty ? (
+          <div className="flex flex-col items-center justify-center hover:bg-blue-300 bg-gray-300 rounded-xl mt-5 p-4 md:w-full h-24 cursor-pointer">
+            <img src={emptyCart} alt="empty cart" className="w-10 h-10" />
+            <div>Cart Empty!</div>
           </div>
-
-          {/* item 2 (description) */}
-          <div className="flex-shrink-0 ">
-            <div>{cart.product_name}</div>
-            <div>Rp {Number(cart.price).toLocaleString(`id-ID`)}</div>
-          </div>
-
-          {/* item 3 (increment) */}
-          <div class="flex items-center justify-between w-1/4">
-            <button
-              class="flex-shrink-0 w-8 h-8 px-2 py-1 border bg-gray-700 border-gray-600 rounded text-white"
-              onclick="decrement()"
+        ) : (
+          cart.map((cartItem) => (
+            <div
+              key={cartItem.id}
+              className="flex justify-between items-center bg-gray-300 rounded-xl mt-2 p-4 md:w-full h-24"
             >
-              -
-            </button>
-            <span class="flex-shrink-0 w-8 h-8 bg-white flex items-center justify-center font-semibold ml-0.5">
-              1
-            </span>
-            <button
-              class="flex-shrink-0 w-8 h-8 px-2 py-1 border bg-gray-700 border-gray-600 rounded text-white ml-0.5"
-              onclick="increment()"
-            >
-              +
-            </button>
-          </div>
-        </div>
+              {/* ...item content */}
+              <div className="flex-shrink-0">
+                <img
+                  src={cartItem.image}
+                  alt="pho"
+                  class="w-16 h-16 rounded-full"
+                />
+              </div>
 
-        {/* submit button */}
-        <div className=" bg-gray-300 rounded-xl mt-2 p-4 md:w-full h-24">
+              {/* item 2 (description) */}
+              <div className="flex-shrink-0 ">
+                <div>{cartItem.product_name}</div>
+                <div>Rp {Number(cartItem.price).toLocaleString(`id-ID`)}</div>
+              </div>
+
+              <div className="flex items-center justify-between w-1/4">
+                <button
+                  onClick={() => decrementCart(cartItem.id)}
+                  className="flex-shrink-0 w-8 h-8 px-2 py-1 border bg-gray-700 border-gray-600 rounded text-white"
+                >
+                  -
+                </button>
+                <span className="flex-shrink-0 w-8 h-8 bg-white flex items-center justify-center font-semibold ml-0.5">
+                  {cartItem.quantity}
+                </span>
+                <button
+                  onClick={() => incrementCart(cartItem.id)}
+                  className="flex-shrink-0 w-8 h-8 px-2 py-1 border bg-gray-700 border-gray-600 rounded text-white ml-0.5"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          ))
+        )}
+
+        <div
+          className="flex items-center justify-center hover:bg-blue-300 bg-gray-300 rounded-xl mt-5 p-4 md:w-full h-24 cursor-pointer"
+          onClick={submit}
+        >
           Submit
         </div>
       </div>
