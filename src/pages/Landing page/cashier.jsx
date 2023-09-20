11885286d8cd1c46b396b66cc5617b79baed8081
Andre Riyanto <andreriyantoo19@gmail.com>
2023-09-20 14:44:39 +0700

    to cashier

--- src/pages/Landing page/cashier.jsx
+++ src/pages/Landing page/cashier.jsx
@@ -1,11 +1,11 @@
 import { NavTemplateCashier } from "../../components/template/template";
-
+const beHonest = require("../../assets/pictures/behonet-removebg-preview.png");
 export const CashierLandingPage = () => {
   return (
     <>
       <NavTemplateCashier>
-        <div className="center flex justify-center">
-          Welcome to cashier page :)
+        <div className="flex justify-center ">
+          <img src={beHonest} alt="" className="object-cover" />
         </div>
       </NavTemplateCashier>
     </>
