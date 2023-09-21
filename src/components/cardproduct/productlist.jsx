8ae5ffdf4c933cdb67c2811d088d003538e7f78e
Arman Septian Pratama <armansept18@gmail.com>
2023-09-21 11:53:42 +0700

    fix: responsive sorting bar

--- src/components/cardproduct/productlist.jsx
+++ src/components/cardproduct/productlist.jsx
@@ -2,7 +2,18 @@ import { useEffect, useState } from "react";
 import api from "../../api/api";
 import { TiTick } from "react-icons/ti";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import { Button, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useToast,
+} from "@chakra-ui/react";
 
 const CategoryToName = (category_id) => {
   switch (category_id) {
@@ -73,61 +84,51 @@ export const CardProduct = ({ product, onEdit, onDelete, fetchSearch }) => {
     }
   };
   return (
-    <>
-      {product?.product?.map((item, i) => {
-        return (
-          <div
-            className="bg-white flex justify-between w-full p-4  bg-gray-200 mt-2 items-center"
-            style={{ borderRadius: "12px", border: "1px solid black" }}
-            key={i}
-            // onClick={() => toggleStick(i)}
-          >
-            <div className="w-11 h-7 items-center flex">
-              <div
-                className="flex items-center p-1 w-7 h-7"
-                // style={{ border: "2px solid black" }}
+    <Box overflowX="auto">
+      <Table variant={"simple"} size={["sm", "md", "lg"]} spacing={3}>
+        <Tbody>
+          {product?.product?.map((item, i) => (
+            <Tr key={i} className="justify-center">
+              <Td
+                className=" max-[473px]:hidden"
+                width={["110px", "110px"]}
+                minW={["100px", "100px"]}
               >
-                {isTickVisible[i] && <TiTick />}
-              </div>
-              <img
-                className="object-fill w-12 h-10"
-                style={{
-                  boxShadow: "1px 2px 3px black",
-                  border: "1px solid white",
-                  // marginLeft: "32px",
-                }}
-                // src={item.image}
-                src={`http://localhost:2000/static/${item.image}`}
-                alt=""
-              />
-            </div>
-            <div className="flex items-center">
-              <span>{item.product_name}</span>
-              <hr style={{ color: "black" }} />
-            </div>
-            <div className="flex items-center">
-              <span>{CategoryToName(item.category_id)}</span>
-              <hr style={{ color: "black" }} />
-            </div>
-            <div className="flex items-center">
-              <span>{formatIdr(item.price)}</span>
-              <hr style={{ color: "black" }} />
-            </div>
-            <div className="flex items-center">
-              <span>{item.stock}</span>
-              <hr style={{ color: "black" }} />
-            </div>
-            <div className="flex items-center gap-3">
-              <EditIcon cursor={"pointer"} onClick={() => onEdit(item)} />
-              <DeleteIcon
-                cursor={"pointer"}
-                onClick={() => handleDelete(item)}
-              />
-              <hr style={{ color: "black" }} />
-            </div>
-          </div>
-        );
-      })}
-    </>
+                <img
+                  src={`http://localhost:2000/static/${item.image}`}
+                  className="object-fill w-12 h-12 rounded-xl"
+                  style={{
+                    boxShadow: "1px 2px 3px black",
+                    border: "1px solid white",
+                  }}
+                  alt=""
+                />
+              </Td>
+              <Td width={["50px", "200px"]} minW={["50px", "200px"]}>
+                {item.product_name}
+              </Td>
+              <Td width={["50px", "200px"]} minW={["50px", "200px"]}>
+                {CategoryToName(item.category_id)}
+              </Td>
+              <Td width={["50px", "100px"]} minW={["50px", "100px"]}>
+                {formatIdr(item.price)}
+              </Td>
+              <Td width={["50px", "100px"]} minW={["50px", "100px"]}>
+                {item.stock}
+              </Td>
+              <Td width={["50px", "50px"]} minW={["50px", "50px"]}>
+                <EditIcon cursor={"pointer"} onClick={() => onEdit(item)} />
+              </Td>
+              <Td width={["50px", "50px"]} minW={["50px", "50px"]}>
+                <DeleteIcon
+                  cursor={"pointer"}
+                  onClick={() => handleDelete(item)}
+                />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   );
 };
