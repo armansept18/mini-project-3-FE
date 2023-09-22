5df58efdfdfda8e56ecbcca88fc35b6bb31cb540
nazhifsetya27 <nazhif.nugroho@gmail.com>
2023-09-22 09:07:33 +0700

    finalize pt 2

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-mini-project-3.nazhifsetya.site",
+  baseURL: process.env.REACT_APP_api_url,
 });
 
 export default api;
